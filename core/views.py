ec4df4700874f98a976c6c56ffd4a66ead5435ca
HubertFijolek1 <hubertfijolek1@gmail.com>
2024-12-05 12:30:34 +0100

    Add JWT authentication and protected view
    Integrate JWT settings for access and refresh token lifetimes in settings.py. Introduce a new protected view in views.py requiring authentication.

--- core/views.py
+++ core/views.py
@@ -1,5 +1,9 @@
 from rest_framework import generics
 from .serializers import UserRegistrationSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class UserRegistrationView(generics.CreateAPIView):
     serializer_class = UserRegistrationSerializer
+
+class ProtectedView(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
