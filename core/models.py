1774b357813ba9bb131de6acba3dd144ed21bcf7
HubertFijolek1 <hubertfijolek1@gmail.com>
2024-12-05 12:53:34 +0100

    Add user profiles and media file handling
    Introduce the UserProfile model linked to Django's User model to allow each user to have a profile with an avatar and bio. Implement signals to automatically create or update the user profile upon user creation. Configure media settings and update URLs for media file handling using the Pillow library for image processing.

--- core/models.py
+++ core/models.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
@@ -7,3 +10,11 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s profile"
