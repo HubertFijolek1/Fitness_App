22fa8184830486be818e19dfc54acc8fe8b6ba46
HubertFijolek1 <hubertfijolek1@gmail.com>
2024-12-05 12:58:31 +0100

    Add AWS S3 storage configuration
    Integrate AWS S3 for media file storage by adding 'django-storages' and 'boto3' to requirements. Update settings to include AWS credentials and configure the default file storage backend. This change enables efficient and scalable media file management in the application.

--- fitness_tracker/settings.py
+++ fitness_tracker/settings.py
@@ -49,6 +49,17 @@ AUTH_USER_MODEL = 'core.CustomUser'
 
 # Application definition
 
+# Configure AWS settings
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME', 'us-east-1')
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+# Configure media files storage
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -59,6 +70,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'core',
+    'storages',
 ]
 
 MIDDLEWARE = [
