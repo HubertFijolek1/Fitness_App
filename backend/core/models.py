8e762eda9efb5e3f2df99f4643e7ff57bda4d0d1
HubertFijolek1 <hubertfijolek1@gmail.com>
2024-12-06 16:22:50 +0100

    Add Exercise model (type, duration, sets, date)

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
 
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
@@ -14,3 +15,14 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return f"{self.user.username}'s profile"
+
+class Exercise(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    exercise_type = models.CharField(max_length=100)
+    duration_minutes = models.IntegerField()
+    sets = models.IntegerField(default=1)
+    reps = models.IntegerField(default=0)
+    date = models.DateField()
+
+    def __str__(self):
+        return f"{self.user.username} - {self.exercise_type} on {self.date}"
